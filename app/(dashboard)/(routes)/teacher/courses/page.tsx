c8d3c091328119cbfc5e7f995734d5c4448256ba
Hein Htet Zaw <bogeyhz.325@gmail.com>
2024-01-27 18:33:16 +0630

    Course datatable filter sorting ui logic finished

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,14 +1,37 @@
-import { Button } from '@/components/ui/button'
-import Link from 'next/link'
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import React from "react";
+import { DataTable } from "./_components/data-table";
+import { columns } from "./_components/columns";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { db } from "@/lib/db";
 
-const CoursesPage = () => {
+
+
+const CoursesPage = async () => {
+  const { userId } = auth();
+  if (!userId) {
+    return redirect("/");
+  }
+  const courses = await db.course.findMany({
+    where: {
+      userId:userId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
   return (
-    <div className=' p-6'>
-        <Link href={'/teacher/create'}>
-        <Button variant={'default'}>Create Course</Button></Link>
+    <div className=" p-6">
+      {/* <Link href={"/teacher/create"}>
+        <Button variant={"default"}>Create Course</Button>
+      </Link>
+       */}
+      <DataTable columns={columns} data={courses} />
     </div>
-  )
-}
+  );
+};
 
-export default CoursesPage
+export default CoursesPage;
