0a6940c330429fce9b90f9ccd4f8c395c75e8da0
Hein Htet Zaw <bogeyhz.325@gmail.com>
2024-02-06 21:09:07 +0630

    Stripe integration finished

--- app/(course)/courses/[courseId]/chapters/[chapterId]/_components/course-enroll-button.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/_components/course-enroll-button.tsx
@@ -1,14 +1,32 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import { format } from "@/lib/format";
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
+import toast from "react-hot-toast";
 interface CourseEnrollButtonProps {
   courseId: string;
   price: number;
 }
-const CourseEnrollButton = ({courseId,price}:CourseEnrollButtonProps) => {
-  return <Button size={'sm'} className=" w-full md:w-auto">
-    Enroll For {format(price)}
-  </Button>;
+const CourseEnrollButton = ({ courseId, price }: CourseEnrollButtonProps) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const onClick = async () => {
+    try {
+      setIsLoading(true);
+      const res = await axios.post(`/api/courses/${courseId}/checkout`);
+      window.location.assign(res.data.url);
+    } catch (error) {
+      toast.error("Something went wrong.");
+      console.log(error)
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  return (
+    <Button disabled={isLoading} size={"sm"} className=" w-full md:w-auto" onClick={onClick}>
+      Enroll For {format(price)}
+    </Button>
+  );
 };
 
 export default CourseEnrollButton;
